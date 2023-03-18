bec1dea8fc9ababc0f2ac79fbea7e4366154be70
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-03-18 19:58:28 +0300

    Add reviews section.

---
+++ src/data/reviews.js
@@ -0,0 +1,20 @@
+export const reviews = [
+  {
+    reviewer: "Derek",
+    course: "Common Irish",
+    rating: 4,
+    text: "I really enjoy working with you. In 3 months I picked up my Irish and learned how to easily carry on conversations in it. Excellent teachers! I am satisfied with everything.",
+  },
+  {
+    reviewer: "Nina",
+    course: "General Irish",
+    rating: 5,
+    text: "I have been studying at school for 3 months now and I see tangible results: vocabulary is bigger, grammar is better. I really like my teacher. I myself chose it according to the description / greeting and after listening to the audio. I was looking for a native speaker teacher, preferably with British English.",
+  },
+  {
+    reviewer: "John",
+    course: "Spoken Irish",
+    rating: 5,
+    text: 'I started training a couple of months ago to get a job and successfully pass an interview. Everything worked out)) It was important for me to improve my "listening" skills and grammar + we rehearsed with the teacher my story about myself and my work. They hired me and I\'m still doing it. All good)',
+  },
+];
