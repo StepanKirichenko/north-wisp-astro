f15ac80a4422a49352c58d52bc5e98d668b457cb
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-03-18 20:10:23 +0300

    Add team memeber data.

---
+++ src/data/team.js
@@ -0,0 +1,23 @@
+export const team = [
+  {
+    name: "Emily Arthur",
+    job: "Education Manager",
+    image: "emily_arthur.png",
+    description:
+      "Emily is from County Armagh. She received her bachelor's degree in Irish and French from Queen's University Belfast and completed her first degree in both subjects in 2015. She then completed her MA and PhD in Irish from the same institution, completing her PhD in 2020. She has a special interest. in contemporary Irish literature, especially travel and emigration literature.",
+  },
+  {
+    name: "John Brian",
+    job: "Head of Curriculum Design",
+    image: "john_brian.png",
+    description:
+      "Emily is from County Armagh. She received her bachelor's degree in Irish and French from Queen's University Belfast and completed her first degree in both subjects in 2015. She then completed her MA and PhD in Irish from the same institution, completing her PhD in 2020. She has a special interest. in contemporary Irish literature, especially travel and emigration literature.",
+  },
+  {
+    name: "Emma Bailey",
+    job: "Irish Language Teacher",
+    image: "emma_bailey.png",
+    description:
+      "Emily is from County Armagh. She received her bachelor's degree in Irish and French from Queen's University Belfast and completed her first degree in both subjects in 2015. She then completed her MA and PhD in Irish from the same institution, completing her PhD in 2020. She has a special interest. in contemporary Irish literature, especially travel and emigration literature.",
+  },
+];
